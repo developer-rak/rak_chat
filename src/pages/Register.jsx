4250fd41ae0425b319ce684162bd62021160cc99
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-04-24 14:40:22 +0500

    register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../style.scss';
 
+import Add from '../assets/addAvatar.png';
+
 const Register = () => {
   return (
     <div className='formContainer'>
@@ -8,10 +10,14 @@ const Register = () => {
          <span className='logo'>rak Chat</span>
          <span className='title'>Register</span>
          <form>
-            <input type="text" placeholder='display name' />
+            <input type="text" placeholder='name' />
             <input type="email" placeholder='email' />
             <input type="password" placeholder='password' />
-            <input type="file" />
+            <input style={{ display: 'none' }} type="file" id='file' />
+            <label htmlFor="file">
+               <img src={Add} alt="Avatar" />
+               <span>add an avatar</span>
+            </label>
             <button>Sign up</button>
          </form>
          <p>You do have an account? Login</p>
