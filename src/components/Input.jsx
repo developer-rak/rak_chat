0147133060f9b5597cd0be56d11988e9565dff7b
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-04-26 11:58:59 +0500

    message left right

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Img from '../assets/img.png';
+import Attach from '../assets/attach.png';
 
 const Input = () => {
   return (
-    <div>
-      Input
+    <div className='input'>
+      <input type="text" placeholder='Type something...' />
+      <div className="send">
+        <img src={Attach} alt="file" />
+        <input type="file" style={{ display: 'none' }} id='file' />
+        <label htmlFor="file">
+          <img src={Img} alt="attach" />
+        </label>
+        <button>Send</button>
+      </div>
     </div>
   )
 }
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>
-      Message
+    <div className='message owner'>
+      <div className="messageInfo">
+        <img src="https://images.pexels.com/photos/19554319/pexels-photo-19554319/free-photo-of-young-woman-in-a-graduation-gown-and-mortarboard-standing-outside.jpeg?auto=compress&cs=tinysrgb&w=600" alt="profile" />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img src="https://images.pexels.com/photos/19554319/pexels-photo-19554319/free-photo-of-young-woman-in-a-graduation-gown-and-mortarboard-standing-outside.jpeg?auto=compress&cs=tinysrgb&w=600" alt="contentImg" />
+      </div>
     </div>
   )
 }
