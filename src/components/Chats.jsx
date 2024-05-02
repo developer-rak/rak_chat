2534f1642886e5af11f1410bcf6a3c5634f91599
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-05-02 17:36:28 +0500

    chat collection

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { AuthContext } from '../context/AuthContext';
 import { doc, onSnapshot } from 'firebase/firestore';
+import { AuthContext } from '../context/AuthContext';
+import { ChatContext } from '../context/ChatContext';
 import { db } from '../firebase';
 
 const Chats = () => {
   const [chats, setChats] = useState([]);
 
-  const { currentUser } = useContext(AuthContext)
+  const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
 
   useEffect(() => {
     const getChats = () => {
@@ -22,10 +24,18 @@ const Chats = () => {
     currentUser.uid && getChats()
   }, [currentUser.uid]);
 
+  const handleSelect = (u) => {
+    dispatch({type: "CHANGE_USER", payload: u})
+  }
+
   return (
     <div className='chats'>
       {Object.entries(chats)?.map((chat) => (
-        <div className="userChat" key={chat[0]}>
+        <div 
+          className="userChat" 
+          key={chat[0]} 
+          onClick={()=> handleSelect(chat[1].userInfo)}
+        >
           <img src={chat[1].userInfo.photoURL} alt="user" />
           <div className="userChatInfo">
             <span>{chat[1].userInfo.displayName}</span>
