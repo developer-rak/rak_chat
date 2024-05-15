1d1e1545a8eb79cc431edf8de9037185202bd016
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-05-15 11:33:09 +0500

    added user chats to firebase

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import Message from './Message';
+import { ChatContext } from '../context/ChatContext';
+import { onSnapshot } from 'firebase/firestore';
+import { doc } from 'firebase/firestore';
+import { db } from '../firebase';
 
 const Messages = () => {
+  const [ messages, setMessages ] = useState([])
+  const { data } = useContext(ChatContext);
+
+  useEffect = (() => {
+    const unSub = onSnapshot(doc(db,"chats", data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data())
+    })
+
+    return () => {
+      unSub()
+    }
+  }, [data.chatId])
+
   return (
     <div className='messages'>
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {messages.map((m) => (
+        <Message message={m} />
+      ))}
     </div>
   )
 }
-
 export default Messages;
